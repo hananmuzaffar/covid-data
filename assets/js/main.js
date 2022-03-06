cf34b8448d353186c8bf6488ce1ea3715c9e2cfb
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-03-06 09:47:21 +0530

    assets updation

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector("#progress").innerHTML = '<div class="progress"><div class="indeterminate"></div></div><p class="center" style="font-weight:bold">Working on it.</p>';
+
+document.querySelector("#copyright").innerHTML = '&#169; 2020 J&amp;K COVID-19 Data';
+document.querySelector("#designer").innerHTML = 'Designed by <a class="grey-text text-lighten-3" href="//www.instagram.com/_hananmuzaffar_" target="_blank" >Hanan Muzaffar</a>';
+// $(document).ready(function(){
+//         $(".button-collapse").sideNav();
+// });

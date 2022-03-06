1f931460f06aae84c03e8b96c52ac551ff1410cd
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-03-06 09:44:11 +0530

    assets updation

---
+++ default.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>403 Access Denied</title>
+        <meta http-equiv="refresh" content="2;url=http://biblichor.co.in" />
+    </head>
+    <body>
+        <h1>403 Access Denied</h1>
+        <p>You do not have permission to view this folder</p>
+    </body>
+</html>

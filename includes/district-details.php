ff71eb24f3ef8be5e449776770adcb3c0bfb1a30
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-03-08 20:56:55 +0530

    Added if..else...elseif statement
    Added if..else...elseif statement to add upward and downward arrow for positive and negative integer respectively in the string.

--- includes/district-details.php
+++ includes/district-details.php
@@ -15,10 +15,14 @@
                echo
         "<tr id='" . $row['district_name'] . "'>
             <td class='green white-text' style='font-weight:bold'>" . $row['district_name'] . "</td>
-            <td class='black-text text-darken-3'>" . $row['total_positive_cases'] . " <span class='red-text'>&#8593;" . $row['new_positive_cases'] . "</span></td>
-            <td class='black-text text-darken-3'>" . $row['total_active_cases'] . "</td>
-            <td class='black-text text-darken-3'>" . $row['total_recovered'] . " <span class='green-text'>&#8593;" . $row['new_recovered'] . "</span></td>
-            <td class='black-text text-darken-3'>" . $row['total_deaths'] . " <span class='grey-text'>&#8593;" . $row['new_deaths'] . "</span></td>
+            <td class='black-text text-darken-3'>" . $row['total_positive_cases'] . " <span class='red-text'>"; if ($row['new_positive_cases'] > "0"){echo "&#8593; " . $row['new_positive_cases'] . "";} elseif ($row['new_positive_cases'] < "0"){echo "&#8595;" . $row['new_positive_cases'] . "";}
+else {echo "";} echo "</span></td>
+            <td class='black-text text-darken-3'>" . $row['total_active_cases'] . " <span class='blue-text'>"; if ($row['new_active_cases'] > "0"){echo "&#8593; " . $row['new_active_cases'] . "";} elseif ($row['new_active_cases'] < "0"){echo "&#8595;" . $row['new_active_cases'] . "";}
+else {echo "";} echo "</span></td>
+            <td class='black-text text-darken-3'>" . $row['total_recovered'] . " <span class='green-text'>"; if ($row['new_recovered'] > "0"){echo "&#8593; " . $row['new_recovered'] . "";} elseif ($row['new_recovered'] < "0"){echo "&#8595;" . $row['new_recovered'] . "";}
+else {echo "";} echo "</span></td>
+            <td class='black-text text-darken-3'>" . $row['total_deaths'] . " <span class='grey-text'>"; if ($row['new_deaths'] > "0"){echo "&#8593; " . $row['new_deaths'] . "";} elseif ($row['new_deaths'] < "0"){echo "&#8595;" . $row['new_deaths'] . "";}
+else {echo "";} echo "</span></td>
         </tr>
         "; }
         ?>

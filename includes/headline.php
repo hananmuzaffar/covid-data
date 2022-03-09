55f4bd2e47e8267a530e6c3f1f07291c6a390461
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-03-09 18:06:20 +0530

    Minor updates

--- includes/headline.php
+++ includes/headline.php
@@ -1,6 +1,6 @@
 <div class="row">
   <?php
-  require 'fetch-data.php';
+  require './config/fetch-data.php';
   echo"<marquee class='col s12 m12 l12 teal lighten-1 white-text card-panel hoverable' scrollamount='9' onmouseover='this.stop();' onmouseout='this.start();'><h6><strong>" . $positive_new . " new positive cases, " . $recovered_new . " new recoveries &amp; " . $deaths_new . " new death(s) have been reported in Jammu &amp; Kashmir on " . $headline_update . ".</strong></h6></marquee>";
   ?>
 </div>
--- includes/main-data.php
+++ includes/main-data.php
@@ -1,9 +1,9 @@
-    <div class="row deep-purple lighten-5" id="jk">
+    <div class="row deep-purple lighten-5" style="border-radius:2px" id="jk">
         <h5 class="deep-purple lighten-1 white-text jk-heading" style="padding:10px 0 10px 10px; border-radius:2px;">Jammu &amp; Kashmir</h5>
       <?php include 'updation.php' ?>
         <div style="text-transform:uppercase;">
         <?php
-  			include 'fetch-data.php';
+  			include './config/fetch-data.php';
                 echo
             "<div class='col s12 m6 l3 card-panel hoverable red lighten-4 collection-item red-text text-darken-3'><i class='fas fa-users'></i> <span style='font-size:1.25rem'>Positive</span><span class='new badge red white-text' data-badge-caption='new'>" . $positive_new . "</span><p style='font-size: 2rem;'><strong>" . $positive_total . "</strong></p></div>
             <div class='col s12 m6 l3 card-panel hoverable blue lighten-4 collection-item blue-text text-darken-3'><i class='fas fa-procedures'></i>  <span style='font-size:1.25rem'>Active</span><span class='new badge blue white-text' data-badge-caption='new'>" . $active_new . "</span><p style='font-size: 2rem;'><strong>" . $active_total . "</strong></p></div>
@@ -12,12 +12,3 @@
             ?>
             </div>
     </div>
-    <!-- <div class="row yellow lighten-4" id="india">
-            <h5>India</h5>
-            <div style="text-transform:uppercase;">
-                <div class="col s12 m6 l3 card-panel hoverable red lighten-4 collection-item red-text text-darken-3"><i class="fas fa-users"></i> Positive<span class="new badge red white-text" data-badge-caption="new">--</span><p style="font-size: 2rem;"><strong>1,32,68,537</strong></p></div>
-                <div class="col s12 m6 l3 card-panel hoverable blue lighten-4 collection-item blue-text text-darken-3"><i class="fas fa-procedures"></i> Active <p style="font-size: 2rem;"><strong>10,83,303</strong></p></div>
-                <div class="col s12 m6 l3 card-panel hoverable green lighten-4 collection-item green-text text-darken-3"><i class="fas fa-heart"></i> Recovered<span class="new badge green white-text" data-badge-caption="new">--</span><p style="font-size: 2rem;"><strong>1,20,11,124</strong></p></div>
-                <div class="col s12 m6 l3 card-panel hoverable grey lighten-2 collection-item grey-text text-darken-3"><i class="fas fa-ambulance"></i>  Total Deaths<span class="new badge grey white-text" data-badge-caption="new">--</span><p style="font-size: 2rem;"><strong>1,68,719</strong></p></div>
-        </div>
-    </div> -->

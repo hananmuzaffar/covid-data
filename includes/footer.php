bf3d42e2856e9e8a518c06c3d692b2fdb1906c9e
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-03-12 11:39:12 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,24 @@
 <footer class="page-footer" style="margin-top:20px">
     <div class="container">
         <div class="row">
-            <div class="col l6 s12">
+            <div class="col l5 s12">
                 <h5 class="white-text">J&amp;K COVID-19 DATA</h5>
             </div>
             <div class="col l3 s6">
-                <h5 class="white-text">Links</h5>
+                <h5 class="white-text">Quick Links</h5>
                 <ul>
                     <li><a class="grey-text text-lighten-3" href="/"><i class="fas fa-home"></i> Home</a></li>
                     <li><a class="grey-text text-lighten-3" href="sources.php"><i class="fas fa-database"></i> Sources</a></li>
                     <li><a class="grey-text text-lighten-3" href="details.php"><i class="fas fa-users"></i> Patient Details</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-question"></i> Link 4</a></li>
                 </ul>
             </div>
-            <div class="col l3 s6">
-                <h5 class="white-text">Links</h5>
+            <div class="col l4 s6">
+                <h5 class="white-text">Contact</h5>
                 <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-question"></i> Link 1</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-question"></i> Link 2</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-question"></i> Link 3</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-question"></i> Link 4</a></li>
+                    <li><a class="grey-text text-lighten-3" href="mailto:contact@biblichor.co.in" target="_blank"><i class="fa-solid fa-envelope"></i> contact[at]biblichor[dot]co[dot]in</a></li>
+                    <li><a class="grey-text text-lighten-3" href="//www.facebook.com/biblichor.official" target="_blank"><i class="fa-brands fa-facebook"></i> Facebook</a></li>
+                    <li><a class="grey-text text-lighten-3" href="//www.instagram.com/biblichor.official" target="_blank"><i class="fa-brands fa-instagram"></i> Instagram</a></li>
+                    <li><a class="grey-text text-lighten-3" href="//www.twitter.com/Biblichor2" target="_blank"><i class="fa-brands fa-twitter"></i> Twitter</a></li>
                 </ul>
             </div>
         </div>

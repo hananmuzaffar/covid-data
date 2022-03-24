2e3261375b195b4fcc1619a4acf64c91327ed267
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-03-24 20:22:46 +0530

    Update headline.php

--- includes/headline.php
+++ includes/headline.php
@@ -1,6 +1,16 @@
 <div class="row">
   <?php
   require './config/fetch-data.php';
-  echo"<marquee class='col s12 m12 l12 teal lighten-1 white-text card-panel hoverable' scrollamount='9' onmouseover='this.stop();' onmouseout='this.start();'><h6><strong>" . $positive_new . " new positive cases"; if ($deaths_new > 0) { echo ", ";} else { echo " &amp; ";} echo $recovered_new . " new recoveries"; if ($deaths_new > 0) { echo " &amp; " . $deaths_new . " new death(s)";} /*if ($deaths_new > 1) { echo "s "; }*/ else { echo " ";} echo "have been reported in Jammu &amp; Kashmir on " . $headline_update . ".</strong></h6></marquee>";
+  echo"<marquee class='col s12 m12 l12 teal lighten-1 white-text card-panel hoverable' scrollamount='7' onmouseover='this.stop();' onmouseout='this.start();'><h6><strong>" . $NEW_POSITIVE . " new positive cases";
+  if ($NEW_DEATHS > 0)
+  { echo ", "; }
+  else
+  { echo " &amp; "; }
+  echo $NEW_RECOVERED . " new recoveries";
+  if ($NEW_DEATHS > 0)
+  { echo " &amp; " . $NEW_DEATHS . " new death(s) "; }
+  /*if ($NEW_DEATHS > 1) { echo "s "; }*/
+  else { echo " "; }
+  echo "have been reported in Jammu &amp; Kashmir on " . $HEADLINE_UPDATION . ".</strong></h6></marquee>";
   ?>
 </div>

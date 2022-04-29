77aaf5b199780008aae280e48491492e5cafb7fa
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2022-04-29 14:44:27 +0530

    Update sources.php

--- sources.php
+++ sources.php
@@ -1,8 +1,10 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
     <head>
-        <title>Sources | J&amp;K COVID-19 DATA BANK</title>
-        <?php include './includes/meta-tags.php' ?>
+      <?php
+      $page_name = "Sources";
+      echo"<title>$page_name | J&amp;K COVID-19 DATA BANK</title>";
+      include './includes/meta-tags.php' ?>
     </head>
     <body>
         <?php include './includes/header.php';
@@ -14,17 +16,13 @@
                     <div class="row">
                         <h4 class="center-align" style="font-weight:bold; color:#fa6161">Our Sources</h4>
                     </div>
-			        <div class="collection">
-				        <div><a href="//twitter.com/diprjk" target="_blank" class="collection-item">Twitter handle of Department of Information and Public Relations, J&amp;K</a></div>
-				        <div><a href="//covid19bharat.org" target="_blank" class="collection-item">Coronavirus Outbreak in India - covid19bharat.org</a></div>
-			        </div>
-                    <!-- <p>Adding soon...</p>
-                    <div class="progress">
-                        <div class="indeterminate"></div>
-                    </div> -->
-                </div>
-            </div>
+			<div class="collection">
+				<div><a href="//twitter.com/diprjk" target="_blank" class="collection-item">Twitter handle of Department of Information and Public Relations, J&amp;K</a></div>
+				<div><a href="//covid19bharat.org" target="_blank" class="collection-item">Coronavirus Outbreak in India - covid19bharat.org</a></div>
+			</div>
+		    </div>
+		</div>
 	    </div>
-        <?php include './includes/footer.php' ?>
-    </body>
+	    <?php include './includes/footer.php' ?>
+	</body>
 </html>
